947a30e65ffd3d7e9ddcd65827d6605fcffb5223
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-04-04 17:07:19 -0300

    Add table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
+import Table from './components/Table/Table'
 
 function App() {
 
   return (
-    <div className="App">
-     Hello World
-    </div>
+    <Table  />
   )
 }
 

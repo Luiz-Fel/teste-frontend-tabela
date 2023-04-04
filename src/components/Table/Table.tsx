c9094f3bbae9d4bb99488e1fdf5dde3e2a978e27
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-04-04 18:57:43 -0300

    Add table comments

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -6,20 +6,23 @@ import { TableComponent } from "./styles";
 
 function Table({ tableData: args }: TableProps) {
   const { data, headers } = args;
-
+   // Declare state variables for managing the table.
   const [tableData, setTableData] = useState<TableRowProps[]>(data);
   const [selectedColumn, setSelectedColumn] = useState<string>("");
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
-
+  
+  // Function to sort the table by column.
   const sortTable = (columnName: string) => {
     const sortedData = [...tableData];
+    /*If the column that was clicked is the same as the selected column,
+     reverse the sort direction*/
     if (selectedColumn === columnName) {
       setSortDirection(sortDirection === "asc" ? "desc" : "asc");
       return setTableData(sortedData.reverse());
     }
 
-    setSelectedColumn(columnName);
-    sortedData.sort((a: TableRowProps, b: TableRowProps) =>
+    setSelectedColumn(columnName); // Set the selected column to the column that was clicked
+    sortedData.sort((a: TableRowProps, b: TableRowProps) => // Sort the table data by the selected column
       a[columnName as keyof TableRowProps] >
       b[columnName as keyof TableRowProps]
         ? 1

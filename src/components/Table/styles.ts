9fa2b9a0a7cc7883ac4270c7c3a3ed4651527e53
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-04-04 18:53:00 -0300

    Add responsive styles

--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -1,14 +1,35 @@
 import styled from "styled-components";
 
-
 export const TableComponent = styled.table`
-    width: 100%;
-    max-width: 100%;
-    display: block;
-    overflow-x: auto;
-    table-layout: fixed;
-    border-collapse: collapse;
-    border-spacing: 0;
-    border: 1px solid #ddd;
+  width: 100%;
+  max-width: 100%;
+  display: block;
+  overflow-x: auto;
+  table-layout: fixed;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: 1px solid #ddd;
 
-`
+  @media (max-width: 767px) {
+    td {
+      padding: 0.8rem 0.625rem;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1023px) {
+    td {
+      padding: 0.8rem 0.2rem;
+    }
+    th {
+      word-wrap: break-word;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    display: table;
+    font-size: 1.125rem;
+    td {
+      padding: 0.9375rem;
+    }
+  }
+`;
